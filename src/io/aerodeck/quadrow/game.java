40adddb1e05dfc8b08f33abf3b5f2fb998576576
Evan Klein <elklein96@gmail.com>
2013-01-18 20:27:19 -0500

    Changed package name

---
+++ src/io/aerodeck/quadrow/game.java
@@ -0,0 +1,5 @@
+package io.aerodeck.quadrow;
+
+public class game {
+
+}
---
+++ src/io/aerodeck/quadrow/login.java
@@ -0,0 +1,34 @@
+package io.aerodeck.quadrow;
+
+import java.util.Arrays;
+import java.util.Vector;
+
+import org.json.JSONArray;
+
+import android.app.Activity;
+import android.util.Log;
+
+public class login extends Activity{
+	
+		public static String main(String name, String username, String email, String initPassword, String initConfirmPassword){
+			
+			String [] array = {"", "", "", ""};
+			String password = "";
+			
+			if(initPassword == initConfirmPassword){
+				password = initPassword;
+			}
+			
+			if (name.length() != 0 || username.length() != 0 || 
+					email.length() != 0 || password.length() != 0){
+        		array[0] = name;
+        		array[1] = username;
+        		array[2] = email;
+        		array[3] = password;
+        	}
+			
+			Vector<String> v = new Vector<String>(Arrays.asList(array));
+			JSONArray jsonArray = new JSONArray(v);
+			return null;
+		}
+}
